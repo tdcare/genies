940eb8aa5dbf30ff0f0612ea81e9d63dc1aaa39b
tzw <tzw@tdcare.cn>
2025-06-29 10:12:39 +0800

    完善宏信息

--- crates/context/src/app_context.rs
+++ crates/context/src/app_context.rs
@@ -85,7 +85,7 @@ impl ApplicationContext {
     }
 
     pub fn new() -> Self {
-        let config = ApplicationConfig::default();
+        let config = ApplicationConfig::from_sources("application.yml").unwrap();
         ApplicationContext {
             keycloak_keys: futures::executor::block_on(async {
                 get_keycloak_keys(&config.keycloak_auth_server_url, &config.keycloak_realm).await
