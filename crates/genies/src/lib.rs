38c3cbc96c28c0778031cc184c1e56af22cc0594
tzw <tzw@tdcare.cn>
2025-07-17 22:49:06 +0800

    genies v1.4.1

--- crates/genies/src/lib.rs
+++ crates/genies/src/lib.rs
@@ -34,7 +34,7 @@ pub use genies_k8s as k8s ;
 #[macro_export]
 macro_rules! pool {
     () => {
-        &mut genies::CONTEXT.rbatis.clone()
+        &mut genies::context::CONTEXT.rbatis.clone()
     };
 }
 /// 获取带守卫功能的事务连接
@@ -83,7 +83,7 @@ macro_rules! config_gateway {
     ($servlet_path:expr) => {
         once_cell::sync::Lazy::new(|| {
             let service_name = $servlet_path.to_string();
-            let mut gateway = genies::CONTEXT
+            let mut gateway = genies::context::CONTEXT
                 .config
                 .gateway
                 .clone()
